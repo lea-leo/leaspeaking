53ccc284a6218a4bffadca11c85e0b6fa3764bf0
lynchmaniac <lynchmaniac@gmail.com>
2016-07-15 18:50:31 +0200

    clustrerisation de l'application

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,50 @@
 "use strict";
 
-import express from "express";
-
-var app = express();
+const cluster = require('cluster');
+const http = require('http');
 
 import * as twitter from "./twitter";
+import * as arduino from "./arduino";
 
-twitter.streamTwitter();
+var clusterTwitter;
+var clusterArduino;
 
-app.listen(8080, () => {console.log("Express start...");});
+if (cluster.isMaster) {
 
+	//console.log("Je suis le master");
+
+	// Message
+	function messageHandler(msg) {
+		if (msg.cmd && msg.cmd == 'notifyRequest') {
+		  numReqs += 1;
+		}
+		if (msg.cmd && msg.cmd == 'tweetReceive') {
+		  //console.log("MESSAGE DU MASTER");
+		  //console.log(msg.tweet);
+		  clusterArduino.send({tweet: msg.tweet});
+		}
+	}
+
+	// Start workers and listen for messages containing notifyRequest
+	clusterTwitter = cluster.fork({workerName: "twitter"});
+	clusterTwitter.on('message', messageHandler);
+
+	clusterArduino = cluster.fork({workerName: "arduino"});
+	clusterArduino.on('message', messageHandler);
+
+} else {
+	if(process.env.workerName=="twitter"){
+		//console.log("Lancement worker twitter : " + cluster.worker.id);
+		twitter.streamTwitter();
+    }
+
+    if(process.env.workerName=="arduino"){
+		//console.log("Lancement worker arduino : " + cluster.worker.id);
+		process.on('message', function(msg) {
+	    	//console.log('Worker ' + process.pid + ' received message from master.', msg.tweet);
+	    	arduino.writeDataOnSerial(msg.tweet); 
+	  	});
+    }
+}
 
 
