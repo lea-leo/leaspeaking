d5a755cfecc126aa081c01aa1a84fbd869d38978
lynchmaniac <lynchmaniac@gmail.com>
2016-07-15 16:14:41 +0200

    Modification du code C

--- arduino/CommunicationArduino/CommunicationArduino.ino
+++ arduino/CommunicationArduino/CommunicationArduino.ino
@@ -1,14 +1,30 @@
-#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27,20,4 );
+#include <Wire.h>
+
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+
+#include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27,20,4);  // set the LCD address to 0x27 for a 16 chars and 2 line display
+#include <Servo.h>
+
+Servo myservo;  // create servo object to control a servo
+// twelve servo objects can be created on most boards
+
+int pos = 180;
+int LOW_POSITION =  180;
+int HIGH_POSITION =  0;
 
 void setup()
 {
   Serial.begin(9600);
   lcd.init(); // initialize the lcd
   lcd.backlight();
-  lcd.print("Setup Arduino"); // Print a message to the LCD.
+  lcd.print((char) 0x5C);
+ // lcd.print("Setup Arduino"); // Print a message to the LCD.
+  myservo.attach(9);
+  myservo.write(LOW_POSITION);
 
 }
 
@@ -16,9 +32,31 @@ void loop()
 {
   if (Serial.available())  {
     delay(100);
-    lcd.clear();
-    lcd.autoscroll();
+    lcd.setCursor(0,0);
     lcd.print(Serial.readString()); 
     Serial.write("DONE");
+    /*for (pos = 20; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+      // in steps of 1 degree
+      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+      delay(40);                       // waits 15ms for the servo to reach the position
+    }
+    for (pos = 180; pos >= 20; pos -= 1) { // goes from 180 degrees to 0 degrees
+      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+      delay(40);                       // waits 15ms for the servo to reach the position
+    }
+    myservo.write(LOW_POSITION);
+    delay(120);*/
+
+    for (pos = 180; pos >= 20; pos -= 1) { // goes from 180 degrees to 0 degrees
+      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+      delay(15);                       // waits 15ms for the servo to reach the position
+    }
+  delay(1000);
+    for (pos = 20; pos <= 180; pos += 1) { // goes from 0 degrees to 180 degrees
+      // in steps of 1 degree
+      myservo.write(pos);              // tell servo to go to position in variable 'pos'
+      delay(20);                       // waits 15ms for the servo to reach the position
+    }
+    
   }
 }
