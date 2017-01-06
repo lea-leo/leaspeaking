264be771ae49354b7db77bd5f9b525f7d83024cb
lynchmaniac <lynchmaniac@gmail.com>
2017-01-06 17:39:19 +0100

    ajout de tests

--- spec/lea_tests/utils.spec.js
+++ spec/lea_tests/utils.spec.js
@@ -14,3 +14,7 @@ describe("isAdmin", function() {
         expect(Utils.isAdmin("lynchmaniacPL")).toBe(true);
     });
 });
+
+    /*static getRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }*/
